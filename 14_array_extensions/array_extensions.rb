d4d07449abef9ffe9959755da4f2cb4548b05ee1
Man4ester <byrkovskiy@yandex.ru>
2012-11-06 16:13:47 +0200

    14_bug

--- 14_array_extensions/array_extensions.rb
+++ 14_array_extensions/array_extensions.rb
@@ -1,13 +1,11 @@
 class Array
+
   def sum 
      self.inject(0){ |result, elem| result + elem}      
   end
   
   def square
-   self.map{|elem| elem = elem**2}
+   self.map{|elem| elem = elem ** 2}
   end  
-  
-  def square!
-       self.map{|elem1| elem1 = elem1**2}
-  end  
-end
+
+  end
