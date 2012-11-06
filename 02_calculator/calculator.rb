fd5ee39f89863ac54dd0e29cef8921f5268329eb
Man4ester <byrkovskiy@yandex.ru>
2012-11-06 15:39:19 +0200

    bug_2

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,12 +1,15 @@
-def add(a,b) 
-	return a+b
+def add(a, b) 
+	a + b
 end
-def subtract(a,b)
-	return a-b
+
+def subtract(a, b)
+	a - b
 end
+
 def sum(a)
 	a.inject(0) { |n, value| n + value }
 end
+
 def factorial(n)
 	if n == 0
                 1
@@ -14,11 +17,13 @@ def factorial(n)
 	     n * factorial(n-1)	
         end
 end
+
 def multiply(a)
 	a.inject(1){ |n, value| n * value }
 end
-def power(a,b)
-	a**b
+
+def power(a, b)
+	a ** b
 end
 
 
